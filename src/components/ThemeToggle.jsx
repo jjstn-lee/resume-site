be1006da9721db5d4bd7cbe27e012acd8853e7ec
justin lee <justin.hisung.lee@gmail.com>
2025-06-25 18:29:05 -0400

    working on darkmode

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,3 @@
+import { Moon, Sun } from "lucide-react";
+import { useState } from "react";
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import { ThemeToggle } from "@/components/ThemeToggle.jsx"
+
+export const Home = () => {
+    return <div className ="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* theme toggle */}
+            <ThemeToggle /> 
+        {/* background effects */}
+
+        {/* navbar */}
+
+        {/* main bar */}
+
+        {/* footer */}
+    </div>
+};
