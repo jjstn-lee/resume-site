df895c460b407fb69b4152d69c2cd33faac43312
justin lee <justin.hisung.lee@gmail.com>
2025-06-26 22:09:47 -0400

    fixed tailwind config; implemented navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,21 @@
-import { ThemeToggle } from "@/components/ThemeToggle.jsx"
+import { ThemeToggle } from "@/components/ThemeToggle.jsx";
+import { Background } from "@/components/Background.jsx";
+import { Navbar } from "@/components/Navbar.jsx";
+import { HeroSection } from "@/components/HeroSection";
 
 export const Home = () => {
     return <div className ="min-h-screen bg-background text-foreground overflow-x-hidden">
         {/* theme toggle */}
-            <ThemeToggle /> 
+        <ThemeToggle /> 
         {/* background effects */}
-
+        
         {/* navbar */}
-
+        <Navbar />
         {/* main bar */}
+        <main>
+            <HeroSection />
 
+        </main>
         {/* footer */}
     </div>
 };
