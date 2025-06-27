f9e9f8dd6304e654d60821a45877f679d82dbee8
justin lee <justin.hisung.lee@gmail.com>
2025-06-27 00:58:20 -0400

    man fuck it im going to sleep

---
+++ src/components/About.jsx
@@ -0,0 +1,6 @@
+import { cn } from "@/lib/utils"
+import { useEffect, useState } from "react";
+
+export const About = () => {
+
+}
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,7 @@ export const HeroSection = () => {
 
         <div className="container max-w-4xl mx-auto text-center z-10">
             <div className="space-y-6">
-                <h1>
+                <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
                     <span> Hi, I'm </span>
                     <span> Justin </span>
                     <span> Lee </span>
